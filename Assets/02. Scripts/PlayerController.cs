b74898d7d9467c1ca95cd2e16e8082f2394f3475
Song Hogeun <102279006+song-Hogeun@users.noreply.github.com>
2025-12-02 22:57:56 +0900

    PlayerController 기반 중간 완성

--- Assets/02. Scripts/PlayerController.cs
+++ Assets/02. Scripts/PlayerController.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 // 필수 컴포넌트
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour, IDamageable
 {
+    // 컴포넌트
     private Rigidbody2D rb;
     private Animator anim;
 
@@ -18,56 +18,101 @@ public class PlayerController : MonoBehaviour, IDamageable
     private readonly string DeathAnimParam = "4_Death";
     private readonly string DebuffAnimParam = "5_Debuff";
 
+    // 기본 스탯
     private float applySpeed;
     private float moveSpeed = 3f;
+    private float maxHP = 100f;
+    private float currentHP;
+    
+    // 공격 관련
     private float attackPower = 5f;
+    private float attackDelay = 2f;
+    private float currentAttackTimer;
 
+    // 상태 변수
     private bool canMove = true;
     private bool isDeath = false;
 
+    #region 생성 주기
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         
         applySpeed = moveSpeed;
+        currentHP = maxHP;
     }
 
     private void Update()
     {
-        
+        CheckMonster();
     }
 
     private void FixedUpdate()
     {
         OnMove();
     }
+    #endregion
 
+    #region 행동 코드
     // 이동 코드
     private void OnMove()
     {
-        if (!canMove) return;
+        if (!canMove)
+        {
+            anim.SetBool(MoveAnimParam, false);    
+            return;
+        }
         
         rb.linearVelocityX = applySpeed;
         anim.SetBool(MoveAnimParam, true);
     }
-
-    // 앞에 몬스터가 있는가
-    private void CheckMonster()
-    {
-        
-    }
-
+    
+    // 공격 함수
     private void OnAttack()
     {
         if (!canMove)
         {
+            rb.linearVelocityX = 0;
             anim.SetTrigger(AttackAnimParam);
         }
     }
+    #endregion
 
+    #region 기타 함수
+    // 앞에 몬스터가 있는가
+    private void CheckMonster()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.right, 1);
+        
+        // 앞에 콜라이더가 있을 시
+        if (hit.collider != null)
+        {
+            // 이동을 멈추고 공격 실행
+            canMove = false;
+            OnAttack();
+        }
+
+        else
+        {
+            canMove = true;
+        }
+    }
+    
+    // 데미지 처리 함수
     public void TakeDamage(float damage)
     {
+        currentHP -= damage;
         
+        anim.SetTrigger(DamageAnimParam);
+
+        if (currentHP <= 0)
+        {
+            isDeath = true;
+            
+            rb.linearVelocityX = 0;
+            anim.SetTrigger(DeathAnimParam);
+        }
     }
+    #endregion
 }
