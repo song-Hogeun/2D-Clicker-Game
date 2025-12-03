da25da3df9d0825c8c82197a473835761a0d7fe3
Song Hogeun <102279006+song-Hogeun@users.noreply.github.com>
2025-12-03 16:19:56 +0900

    전투 시스템 초기 완성

---
+++ Assets/02. Scripts/Constants.cs
@@ -0,0 +1,11 @@
+public class Constants
+{
+    /// <summary>
+    /// Animator 파라미터
+    /// </summary>
+    public static readonly string MoveAnimParam = "1_Move";
+    public static readonly string AttackAnimParam = "2_Attack";
+    public static readonly string DamageAnimParam = "3_Damage";
+    public static readonly string DeathAnimParam = "4_Death";
+    public static readonly string DebuffAnimParam = "5_Debuff";
+}
--- Assets/02. Scripts/EnemyController.cs
+++ Assets/02. Scripts/EnemyController.cs
@@ -1,23 +1,14 @@
-using System;
 using UnityEngine;
 using static Constants;
 
 // 필수 컴포넌트
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour, IDamageable
+public class EnemyController : MonoBehaviour, IDamageable
 {
-    // 플레이어 전투 타입
-    public enum EPlayerType
-    {
-        Warrior,
-        Archor,
-        Wizard
-    }
-    
     // 컴포넌트
     private Rigidbody2D rb;
     private Animator anim;
-
+    
     // 기본 스탯
     private float applySpeed;
     private float moveSpeed = 3f;
@@ -25,49 +16,44 @@ public class PlayerController : MonoBehaviour, IDamageable
     private float currentHP;
     
     // 공격 관련
-    private float attackPower = 15f;
-    private float attackDelay = 2f;
-    private float attackDistance;
+    private float attackPower = 5f;
+    private float attackDelay = 5f;
     private float currentAttackTimer;
 
     // 상태 변수
     private bool canMove = true;
+    private bool isAttack = false;
     private bool isDeath = false;
-
-    // 플레이어 전투 타입
-    [SerializeField] private EPlayerType playerType;
     
     // 감지할 레이어
-    [SerializeField] private LayerMask monsterLayer;
-
+    [SerializeField] private LayerMask playerLayer;
+    
     #region 생성 주기
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
-
-        InitStat(playerType);
         
         applySpeed = moveSpeed;
         currentHP = maxHP;
     }
-
+    
     private void Update()
     {
-        CheckMonster();
+        CheckPlayer();
     }
-
+    
     private void FixedUpdate()
     {
         OnMove();
     }
     #endregion
-
+    
     #region 행동 코드
     // 이동 코드
     private void OnMove()
     {
-        if (!canMove)
+        if (!isAttack)
         {
             anim.SetBool(MoveAnimParam, false);    
             return;
@@ -91,42 +77,16 @@ public class PlayerController : MonoBehaviour, IDamageable
         currentAttackTimer = attackDelay;
     }
     #endregion
-
-    #region 기타 함수
-
-    private void InitStat(EPlayerType playerType)
-    {
-        switch (playerType)
-        {
-            case EPlayerType.Warrior:
-                attackPower = 15f;
-                attackDelay = 1f;
-                attackDistance = 1f;
-                break;
-            
-            case EPlayerType.Archor:
-                attackPower = 50f;
-                attackDelay = 3.5f;
-                attackDistance = 2f;
-                break;
-            
-            case EPlayerType.Wizard:
-                attackPower = 100f;
-                attackDelay = 5f;
-                attackDistance = 3f;
-                break;
-        }
-    }
     
-    // 앞에 몬스터가 있는가
-    private void CheckMonster()
+    #region 기타 함수
+    private void CheckPlayer()
     {
         // 오른쪽 방향으로 Player 레이어만 레이캐스트
         RaycastHit2D hit = Physics2D.Raycast(
             rb.position,
-            Vector2.right,
-            attackDistance,
-            monsterLayer
+            Vector2.left,
+            1f,
+            playerLayer
         );
 
         if (hit.collider != null)
@@ -163,6 +123,6 @@ public class PlayerController : MonoBehaviour, IDamageable
     {
         if (rb == null) return;
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(rb.position, rb.position + Vector2.right * attackDistance);
+        Gizmos.DrawLine(rb.position, rb.position + Vector2.left * 1f);
     }
 }
